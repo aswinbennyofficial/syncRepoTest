4dc08e5aa350f4c0b992c7045b972fcec4bb9154
Aswin Benny <dev@aswinbenny.in>
2025-01-16 21:30:28 +0530

    Added multiply, removed divide

--- main.go
+++ main.go
@@ -6,14 +6,14 @@ func main() {
 	fmt.Println("Hello World")
 }
 
-func divide(a int, b int) int {
-	return a / b
-}
-
 func add(a int, b int) int {
 	return a + b
 }
 
 func subtract(a int, b int) int {
 	return a - b
-}	
+}
+
+func multiply(a int, b int) int {
+	return a * b
+}
